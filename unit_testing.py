715a37032cc0d0cce885ced5a29cb0c76dcc21d9
ShinyMyst <ShinyMyst@gmail.com>
2023-01-23 21:38:19 -0500

    Added more unit tests.

--- unit_testing.py
+++ unit_testing.py
@@ -11,6 +11,24 @@ class TestNumbers(unittest.TestCase):
         for num in range(10, 16):
             self.assertEqual(self.translator.number_to_text(str(num)), self.translator.exceptions.get(str(num)))
 
+    def test_twenties(self):
+        self.assertEqual(self.translator.number_to_text(str('27')), 'veintisiete')
+
+    def test_regular_tens(self):
+        self.assertEqual(self.translator.number_to_text(str('42')), 'cuarenta y dos')
+    
+    def test_one_hundred(self):
+        self.assertEqual(self.translator.number_to_text(str('100')), 'cien')
+    
+    def test_regular_hundreds(self):
+        self.assertEqual(self.translator.number_to_text(str('200')), 'doscientos')
+
+    def test_five_hundred(self):
+        self.assertEqual(self.translator.number_to_text(str('500')), 'quinientos')
+
+    def test_mixed_hundreds(self):
+        self.assertEqual(self.translator.number_to_text(str('138')), 'ciento treinta y ocho')
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
